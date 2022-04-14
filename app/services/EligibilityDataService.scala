f24006dcd57270185817d986f410f9dd2ec1cee4
doug.maxwell <douglasj.maxwell@icloud.com>
2022-04-14 14:36:05 +0100

    SUPP-236[DM] Update fire starter page to specify eligibility status (#22)
    * removed outstanding penalty
    
    * included outstanding penalty

--- app/services/EligibilityDataService.scala
+++ app/services/EligibilityDataService.scala
@@ -26,7 +26,7 @@ import models.{ EligibilityData, InvoicePeriod, OverDuePayments, OverduePayment 
 import moveittocor.corcommon.model.AmountInPence
 import services.EligibilityDataService._
 import testOnly.models.EligibilityError
-import testOnly.models.EligibilityError._
+import testOnly.models.EligibilityError.{ PayeHasDisallowedCharges, _ }
 import uk.gov.hmrc.http.HeaderCarrier
 
 import java.time.LocalDate
@@ -52,15 +52,14 @@ object EligibilityDataService {
   def rejections(rules: EligibilityRules): Either[NonEmptyList[EligibilityError], Unit] = {
     val validAddress: ValidatedResult[Unit] = if (rules.rlsOnAddress) RLSFlagIsSet.invalidNel else unitResult
     val markedAsInsolvent: ValidatedResult[Unit] = if (rules.markedAsInsolvent) PayeIsInsolvent.invalidNel else unitResult
-    val minimumDebtAllowance: ValidatedResult[Unit] = unitResult
     val maxDebtAllowance: ValidatedResult[Unit] = if (rules.maxDebtAllowance) DebtIsTooLarge.invalidNel else unitResult
-    val disallowedChargeLock: ValidatedResult[Unit] = unitResult
+    val disallowedChargeLock: ValidatedResult[Unit] = if (rules.disallowedChargeLock) PayeHasDisallowedCharges.invalidNel else unitResult
     val existingTTP: ValidatedResult[Unit] = if (rules.existingTTP) YouAlreadyHaveAPaymentPlan.invalidNel else unitResult
+    val outstandingPenalty: ValidatedResult[Unit] = if (rules.eligibleChargeType) OutstandingPenalty.invalidNel else unitResult
     val maxDebtAge: ValidatedResult[Unit] = if (rules.maxDebtAge) DebtIsTooOld.invalidNel else unitResult
-    val eligibleChargeType: ValidatedResult[Unit] = if (rules.eligibleChargeType) PayeHasDisallowedCharges.invalidNel else unitResult
     val returnsFiled: ValidatedResult[Unit] = if (rules.returnsFiled) ReturnsAreNotUpToDate.invalidNel else unitResult
-    (validAddress, markedAsInsolvent, minimumDebtAllowance, maxDebtAllowance, disallowedChargeLock,
-      existingTTP, maxDebtAge, eligibleChargeType, returnsFiled).tupled.void.toEither
+    (validAddress, markedAsInsolvent, maxDebtAllowance, disallowedChargeLock,
+      existingTTP, outstandingPenalty, maxDebtAge, returnsFiled).tupled.void.toEither
   }
 
   def rejectionsOf(rules: EligibilityRules): List[EligibilityError] = {
