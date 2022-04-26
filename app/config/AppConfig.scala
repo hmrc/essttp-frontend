b0298f0b08a3c9728b22a0c9b885f83431793727
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-04-26 09:32:45 +0100

    messages for not eligible

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -61,6 +61,8 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
     val exitSurveyUrl: String = s"${BaseUrl.feedbackFrontend}/feedback/$appName"
     val firstPageBackUrl: String = "https://gov.uk"
     val enrolForPayeUrl: String = config.get[String]("govUkUrls.enrolPayeUrl")
+    val extraSupportUrl: String = config.get[String]("govUkUrls.extraSupportUrl")
+    val relayUrl: String = config.get[String]("govUkUrls.relayUrl")
   }
 
   object InterestRates {
