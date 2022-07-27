b535e4af58770431db128006797e200527eff949
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2022-07-27 15:35:15 +0100

    [JB] tidy up the vals in config

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -43,8 +43,8 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
     val feedbackFrontend: String = platformHost.getOrElse(config.get[String]("baseUrl.feedback-frontend"))
     val gg: String = config.get[String]("baseUrl.gg")
     val businessTaxAccountFrontend: String = platformHost.getOrElse(config.get[String]("baseUrl.business-tax-account-frontend"))
-    val timeToPayUrl: String = s"${servicesConfig.baseUrl("time-to-pay")}"
-    val timeToPayEligibilityUrl: String = s"${servicesConfig.baseUrl("time-to-pay-eligibility")}"
+    val timeToPayUrl: String = servicesConfig.baseUrl("time-to-pay")
+    val timeToPayEligibilityUrl: String = servicesConfig.baseUrl("time-to-pay-eligibility")
   }
 
   object Urls {
