55e6c0a2b955813ab3a670d7a02f3f5fa115007a
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2022-08-10 15:00:12 +0100

    clean up unused messages

--- app/messages/DateMessages.scala
+++ app/messages/DateMessages.scala
@@ -19,67 +19,6 @@ import cats.syntax.eq._
 
 object DateMessages {
 
-  //the same as dd MMMM yy
-  def january(day: Int, year: String): Message = Message(
-    english = s"$day January $year",
-    welsh   = s"$day Ionawr $year"
-  )
-
-  def february(day: Int, year: String): Message = Message(
-    english = s"$day February $year",
-    welsh   = s"$day Chwefror $year"
-  )
-
-  def march(day: Int, year: String): Message = Message(
-    english = s"$day March $year",
-    welsh   = s"$day Mawrth $year"
-  )
-
-  def april(day: Int, year: String): Message = Message(
-    english = s"$day April $year",
-    welsh   = s"$day Ebrill $year"
-  )
-
-  def may(day: Int, year: String): Message = Message(
-    english = s"$day May $year",
-    welsh   = s"$day Mai $year"
-  )
-
-  def june(day: Int, year: String): Message = Message(
-    english = s"$day June $year",
-    welsh   = s"$day Mehefin $year"
-  )
-
-  def july(day: Int, year: String): Message = Message(
-    english = s"$day July $year",
-    welsh   = s"$day Gorffenn $year"
-  )
-
-  def august(day: Int, year: String): Message = Message(
-    english = s"$day August $year",
-    welsh   = s"$day Awst $year"
-  )
-
-  def september(day: Int, year: String): Message = Message(
-    english = s"$day September $year",
-    welsh   = s"$day Medi $year"
-  )
-
-  def october(day: Int, year: String): Message = Message(
-    english = s"$day October $year",
-    welsh   = s"$day Hydref $year"
-  )
-
-  def november(day: Int, year: String): Message = Message(
-    english = s"$day November $year",
-    welsh   = s"$day Tachwedd $year"
-  )
-
-  def december(day: Int, year: String): Message = Message(
-    english = s"$day December $year",
-    welsh   = s"$day Rhagfyr $year"
-  )
-
   val monthName: Map[Int, Message] = Map(
     1 -> Message("January"),
     2 -> Message("February"),
