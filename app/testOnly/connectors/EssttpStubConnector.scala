104057b44ef63bfdd8e5733a7190f55cdcf06e86
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2022-09-26 11:04:58 +0100

    [AC] Add separate config for stubs for test-only start page (#160)

--- app/testOnly/connectors/EssttpStubConnector.scala
+++ app/testOnly/connectors/EssttpStubConnector.scala
@@ -16,23 +16,26 @@
 
 package testOnly.connectors
 
-import config.AppConfig
 import essttp.crypto.CryptoFormat
 import essttp.rootmodel.ttp.EligibilityCheckResult
+import play.api.Configuration
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class EssttpStubConnector @Inject() (httpClient: HttpClient, appConfig: AppConfig) {
+class EssttpStubConnector @Inject() (httpClient: HttpClient, config: Configuration) extends ServicesConfig(config) {
 
   implicit val cryptoFormat: CryptoFormat = CryptoFormat.NoOpCryptoFormat
 
-  val ttpEligibilityUrl: String = s"${appConfig.BaseUrl.timeToPayEligibilityUrl}/debts/time-to-pay/eligibility"
+  val stubsBaseUrl: String = baseUrl("essttp-stubs")
+
+  val insertEligibilityDataUrl: String = s"$stubsBaseUrl/debts/time-to-pay/eligibility/insert"
 
   def primeStubs(response: EligibilityCheckResult)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] =
-    httpClient.POST[EligibilityCheckResult, Unit](s"$ttpEligibilityUrl/insert", response)
+    httpClient.POST[EligibilityCheckResult, Unit](insertEligibilityDataUrl, response)
 
 }
