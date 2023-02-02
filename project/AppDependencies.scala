a4b68ae76429921dd102fdcb58bf91e3fbf5d38b
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2023-02-02 12:13:19 +0000

    [JB][SUPP-717] Remove text Payment Support Service from ineligible pages

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import play.core.PlayVersion
 
 object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.12.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.2.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.13.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.3.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
     "com.beachape"      %% "enumeratum-play"               % "1.7.2",
     "org.typelevel"     %% "cats-core"                     % "2.9.0",
