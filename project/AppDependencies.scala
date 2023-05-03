8cb09e29c8776b1797bcedb5784f9f7f2ed13900
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2023-05-03 10:47:30 +0100

    [AC] library upgrades (#333)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"      % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"              % "7.3.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"              % "7.6.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"   % "1.13.0-play-28",
     "com.beachape"      %% "enumeratum-play"                 % "1.7.2",
     "org.typelevel"     %% "cats-core"                       % "2.9.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"      % "1.107.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"      % "1.109.0",
     "uk.gov.hmrc"       %% "emailaddress"                    % "3.8.0"
   // format: ON
   )
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
-    "org.jsoup"               %  "jsoup"                   % "1.15.4",
+    "org.jsoup"               %  "jsoup"                   % "1.16.1",
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.17.0",
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2",
