d6b8a20be287215442dc183541867ee58a1450c6
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2023-07-12 11:39:44 +0100

    [AC] library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,16 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"      % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"              % "7.7.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"              % "7.14.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"   % "1.13.0-play-28",
     "com.beachape"      %% "enumeratum-play"                 % "1.7.2",
     "org.typelevel"     %% "cats-core"                       % "2.9.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"      % "1.110.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"      % "1.111.0",
     "uk.gov.hmrc"       %% "emailaddress"                    % "3.8.0"
   // format: ON
   )
