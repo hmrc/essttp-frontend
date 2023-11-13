bcf9b906625298152c00d2642d34e4f2cc6b5e7f
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2023-11-13 08:51:03 +0000

    [KN][OPS-11282] Dependency update

--- project/SbtUpdatesSettings.scala
+++ project/SbtUpdatesSettings.scala
@@ -10,6 +10,8 @@ object SbtUpdatesSettings {
     (Compile / compile) := ((Compile / compile) dependsOn dependencyUpdates).value,
     dependencyUpdatesFilter -= moduleFilter("org.scala-lang"),
     dependencyUpdatesFilter -= moduleFilter("com.typesafe.play"),
+    // higher version of enumeratum breaks with java.lang.ClassCastException -Nov'23
+    dependencyUpdatesFilter -= moduleFilter("com.beachape", "enumeratum-play"),
     // locked by version of play
     dependencyUpdatesFilter -= moduleFilter("org.scalatestplus.play", "scalatestplus-play")
   )
