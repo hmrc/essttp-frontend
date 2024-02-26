09346d60729c953ecef81bbc51a48b89a7dcb66e
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2024-02-26 13:51:32 +0000

    [KN][OPS-11846] Amend EligibilityRules

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -25,7 +25,7 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"                   % "1.17.2",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.17.0",
-    "org.wiremock"            %  "wiremock-standalone"     % "3.3.1"
+    "org.wiremock"            %  "wiremock-standalone"     % "3.4.2"
   // format: ON
   ).map(_ % Test)
 }
