1b77f152b3c8969603b0e73bc07b6917a6dd6b79
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-07-16 15:39:00 +0100

    [JG] OPS-12438 - Increase Debt to large amount  (#435)
    * [JG] OPS-12438 - Increasing Debt to large amount
    
    * [JG] OPS-12438 - dependencies and tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "10.1.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "2.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "10.4.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.0.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.0",
     "org.typelevel"     %% "cats-core"                               % "2.12.0",
     "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "1.139.0",
@@ -18,13 +18,13 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     // format: OFF
-    "org.scalatest"           %% "scalatest"               % "3.2.18",
+    "org.scalatest"           %% "scalatest"               % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.1",
-    "org.jsoup"               %  "jsoup"                   % "1.17.2",
+    "org.jsoup"               %  "jsoup"                   % "1.18.1",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.18.0",
-    "org.wiremock"            %  "wiremock-standalone"     % "3.6.0"
+    "org.wiremock"            %  "wiremock-standalone"     % "3.8.0"
   // format: ON
   ).map(_ % Test)
 }
