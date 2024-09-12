1f979d7ba7aa7a3ee3b4c88863180d343aaef6dd
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-09-12 17:07:41 +0100

    [JG] OPS-12345 - remove println

--- app/services/TtpService.scala
+++ app/services/TtpService.scala
@@ -184,7 +184,6 @@ class TtpService @Inject() (
 
     ttpConnector.callArrangementApi(arrangementRequest, correlationId)
       .map { response: ArrangementResponse =>
-        println("hello there" + response.toString)
         auditService.auditPaymentPlanSetUp(journey, Right(response))
         response
       }.recover {
