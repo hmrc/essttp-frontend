d0918c8b8bd38d35611997ea05adccc9c8866f26
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2024-09-20 10:08:42 +0100

    update libarires

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,9 +9,9 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "10.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
-    "com.beachape"      %% "enumeratum-play"                         % "1.8.0",
+    "com.beachape"      %% "enumeratum-play"                         % "1.8.1",
     "org.typelevel"     %% "cats-core"                               % "2.12.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "1.155.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "1.156.0",
     "uk.gov.hmrc"       %% "domain-play-30"                          % "10.0.0"
   // format: ON
   )
