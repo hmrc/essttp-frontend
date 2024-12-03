12bcb5c073cd07c00d6b54cca92ec3b7bf41e931
tp10 <1626193+tp10@users.noreply.github.com>
2024-12-03 09:29:31 +0000

    [TP][OPS-13007] - change error URL (#494)
    * [TP][OPS-13007] - change error URL
    
    * [AC][OPS-13073] no assignmentId in pega startCase response (#493)
    
    * [TP][OPS-13007] - Update error code
    
    ---------
    
    Co-authored-by: Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.6.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.7.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.1",
     "org.typelevel"     %% "cats-core"                               % "2.12.0",
@@ -21,10 +21,10 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"               % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.1",
-    "org.jsoup"               %  "jsoup"                   % "1.18.1",
+    "org.jsoup"               %  "jsoup"                   % "1.18.3",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.18.1",
-    "org.wiremock"            %  "wiremock-standalone"     % "3.9.2"
+    "org.wiremock"            %  "wiremock-standalone"     % "3.10.0"
   // format: ON
   ).map(_ % Test)
 }
