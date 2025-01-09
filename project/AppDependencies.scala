f20918932dbf4dc4d22570777f3855ccfbff65e2
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2025-01-09 12:05:36 +0000

    [JG] OPS-13186 - dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.8.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.9.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.1",
     "org.typelevel"     %% "cats-core"                               % "2.12.0",
