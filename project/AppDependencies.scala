63418f11d94767a3e9acf3564152a631700a39c1
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2025-01-22 13:01:55 +0000

    [AC][OPS-13240] pick up addressPostCode->addressPostcode in postcodeHistory (#519)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.6.0"
+  val bootstrapVersion = "9.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.10.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.1",
-    "org.typelevel"     %% "cats-core"                               % "2.12.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "1.176.0",
+    "org.typelevel"     %% "cats-core"                               % "2.13.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "1.177.0",
     "uk.gov.hmrc"       %% "domain-play-30"                          % "10.0.0"
   // format: ON
   )
