d69e256d57ab21282c125052e12b4d2eadd97248
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2025-03-18 10:37:41 +0000

    [AC][OPS-13430] bump up play frontend hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "11.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.2",
     "org.typelevel"     %% "cats-core"                               % "2.13.0",
