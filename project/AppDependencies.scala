04e1b08799a9d8b7f7f58cd651656084007de79d
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2025-05-07 14:22:23 +0100

    [JG] OPS-13585 - memory leak fix (#555)
    * [JG] OPS-13585 fix memory test leak
    
    * [JG] OPS-13585 update cor journey

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.2",
     "org.typelevel"     %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.4.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.5.0",
     "uk.gov.hmrc"       %% "domain-play-30"                          % "12.0.0"
   // format: ON
   )
