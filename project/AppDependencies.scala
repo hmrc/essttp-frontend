b4ae0cea973720996f371d8a120639be64e685e5
Kntanos <kostas@ntanos.co.uk>
2025-07-09 12:51:31 +0100

    [KN][OPS-13851] Dependencies update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "9.16.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "12.6.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "12.7.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.2",
     "org.typelevel"     %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.8.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.9.0",
     "uk.gov.hmrc"       %% "domain-play-30"                          % "12.1.0"
   // format: ON
   )
@@ -20,7 +20,7 @@ object AppDependencies {
     // format: OFF
     "org.scalatest"           %% "scalatest"               % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.1",
+    "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.2",
     "org.jsoup"               %  "jsoup"                   % "1.21.1",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.18.1",
