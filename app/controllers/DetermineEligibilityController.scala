60af26ae71ca9d2a1837e08abe9bdde19d0d29ea
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2025-08-26 12:10:52 +0100

    [AC][OPS-14014] check for MTD enrolment only for MTDITSA (#572)

--- app/controllers/DetermineEligibilityController.scala
+++ app/controllers/DetermineEligibilityController.scala
@@ -26,6 +26,7 @@ import controllers.pagerouters.EligibilityRouter
 import essttp.journey.model.{Journey, JourneyStage}
 import essttp.rootmodel.TaxRegime
 import essttp.rootmodel.TaxRegime.Sa
+import essttp.rootmodel.ttp.CustomerTypes
 import essttp.rootmodel.ttp.eligibility.{EligibilityCheckResult, EligibilityPass, EligibilityStatus}
 import play.api.mvc.*
 import services.{AuditService, JourneyService, TtpService}
@@ -72,11 +73,12 @@ class DetermineEligibilityController @Inject() (
     } yield
       if (eligibilityCheckResult.isEligible) {
         journey.taxRegime match {
-          case Sa if r.enrolments.getEnrolment("HMRC-MTD-IT").isDefined =>
-            mtdEligibleResult(eligibilityCheckResult)
-          case Sa                                                       =>
-            mtdIneligibleResult(eligibilityCheckResult)
-          case _                                                        =>
+          case Sa if eligibilityCheckResult.individualDetails.flatMap(_.customerType).contains(CustomerTypes.MTDITSA) =>
+            if (r.enrolments.getEnrolment("HMRC-MTD-IT").isDefined)
+              mtdEligibleResult(eligibilityCheckResult)
+            else
+              mtdIneligibleResult(eligibilityCheckResult)
+          case _                                                                                                      =>
             eligibilityCheckResult
         }
       } else {
@@ -115,8 +117,9 @@ class DetermineEligibilityController @Inject() (
       case TaxRegime.Sa => r.nino.map(_ => ())
       case _            => Some(())
     }
+
   // if SA users have IR-SA enrollment, but no HMRC-MTD-IT enrollment, they are directed to the 'Sign up tp MDT' kickout page
-  private def mtdIneligibleResult(result: EligibilityCheckResult): EligibilityCheckResult                    =
+  private def mtdIneligibleResult(result: EligibilityCheckResult): EligibilityCheckResult =
     result.copy(
       eligibilityRules = result.eligibilityRules.copy(
         noMtditsaEnrollment = Some(true)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.16.0"
+  val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "12.8.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.8.2",
     "org.typelevel"     %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.10.0",
-    "uk.gov.hmrc"       %% "domain-play-30"                          % "12.1.0"
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.11.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                          % "13.0.0"
   // format: ON
   )
 
@@ -21,7 +21,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"               % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.2",
-    "org.jsoup"               %  "jsoup"                   % "1.21.1",
+    "org.jsoup"               %  "jsoup"                   % "1.21.2",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.18.1",
     "org.wiremock"            %  "wiremock-standalone"     % "3.13.1"
