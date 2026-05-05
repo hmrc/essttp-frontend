0c3cf7a9602a721a91e03d02f96ac4e598bc682b
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2026-05-05 15:09:19 +0100

    [JG] OPS-15013 - removing abbr from NDDS messages

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "13.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"              % "13.4.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.5.0",
     "com.beachape"      %% "enumeratum-play"                         % "1.9.6",
     "org.typelevel"     %% "cats-core"                               % "2.13.0",
-    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.19.0",
+    "uk.gov.hmrc"       %% "essttp-backend-cor-journey"              % "2.20.0",
     "uk.gov.hmrc"       %% "domain-play-30"                          % "13.0.0"
   // format: ON
   )
@@ -21,7 +21,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"               % "3.2.20",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.2",
-    "org.jsoup"               %  "jsoup"                   % "1.22.1",
+    "org.jsoup"               %  "jsoup"                   % "1.22.2",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"              % "1.19.0",
     "org.wiremock"            %  "wiremock-standalone"     % "3.13.2"
